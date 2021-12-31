a04201089fa3456f18983e245ed6b3ab746850e7
Dan Fellner <dfellner92@gmail.com>
2021-12-30 23:39:48 -0500

    truthy falsy notes

--- functions.test.js
+++ functions.test.js
@@ -7,3 +7,10 @@ test("Adds 2 + 2 to equal 4", () => {
 test("Expect answer to not be 5", () => {
   expect(functions.add(2, 2)).not.toBe(5);
 });
+
+// CHECK FOR TRUTHY & FALSY VALUES
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as truth
+// toBeFalsy matches anything that an if statement treats as false
