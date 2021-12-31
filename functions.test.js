db7664a13abb4c4afa6e97cfccc97b8ece33f089
Dan Fellner <dfellner92@gmail.com>
2021-12-30 23:35:36 -0500

    two tests in the suite

--- functions.test.js
+++ functions.test.js
@@ -1,5 +1,9 @@
-const functions = require('./functions')
+const functions = require("./functions");
 
 test("Adds 2 + 2 to equal 4", () => {
   expect(functions.add(2, 2)).toBe(4);
+});
+
+test("Expect answer to not be 5", () => {
+  expect(functions.add(2, 2)).not.toBe(5);
 });
