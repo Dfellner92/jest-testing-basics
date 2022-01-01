f09a183e9261ec389d71261818e861072a3eacc3
Dan Fellner <dfellner92@gmail.com>
2021-12-31 22:38:18 -0500

    non prim checkers

--- functions.test.js
+++ functions.test.js
@@ -14,3 +14,20 @@ test("Expect answer to not be 5", () => {
 // toBeDefined is the opposite of toBeUndefined
 // toBeTruthy matches anything that an if statement treats as truth
 // toBeFalsy matches anything that an if statement treats as false
+
+test("should be null", () => {
+  expect(functions.isNull()).toBeNull();
+});
+
+test("should be falsy", () => {
+  expect(functions.checkValue(0)).toBeFalsy();
+});
+
+// toBe wont work. only for primitives. nonprims, use toEqual
+
+test("User Obj should be Dan Fellner", () => {
+  expect(functions.createUser()).toEqual({
+    firstName: "Dan",
+    lastName: "Fellner",
+  });
+});
